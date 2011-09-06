fb0785f5eb80259ea13d7ca9f0dbc622934b5b9f
Alek Storm <alek.storm@gmail.com>
2011-09-06 06:09:00 +0000

    Add Sphinx options to include both class-level and __init__ docstrings, and to order members by source

--- doc/conf.py
+++ doc/conf.py
@@ -40,6 +40,9 @@ master_doc = 'index'
 project = u'PyGravatar'
 copyright = u'2011, Alek Storm'
 
+autoclass_content = 'both'
+autodoc_member_order = 'bysource'
+
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
