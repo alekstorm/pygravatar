8e9fbbba9c239defa1e58eefe33f538bb9ed9b75
Alek Storm <alek.storm@gmail.com>
2011-09-06 03:06:37 +0000

    Rename to PyGravatar in documentation

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# python-gravatar documentation build configuration file, created by
+# PyGravatar documentation build configuration file, created by
 # sphinx-quickstart on Tue Sep  6 02:08:06 2011.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'python-gravatar'
+project = u'PyGravatar'
 copyright = u'2011, Alek Storm'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -158,8 +158,7 @@ html_static_path = ['_static']
 #html_file_suffix = ''
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'python-gravatardoc'
-
+htmlhelp_basename = project+'doc'
 
 # -- Options for LaTeX output --------------------------------------------------
 
@@ -172,7 +171,7 @@ htmlhelp_basename = 'python-gravatardoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'python-gravatar.tex', u'python-gravatar Documentation',
+  ('index', project+'.tex', project+u' Documentation',
    u'Alek Storm', 'manual'),
 ]
 
